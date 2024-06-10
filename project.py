52cbc67fe5caf3b78fc70ab558d8ee8c55d3927c
michaelkreutz <theflossbros70188@gmail.com>
2024-06-10 14:25:56 -0400

    outline of level 1 complete

--- project.py
+++ project.py
@@ -9,23 +9,25 @@ class Player:
         self.x = x
         self.y = y
         self.screen = screen
+        self.size = 40
 
     def move(self, dx, dy):
         self.x += dx
         self.y += dy
-        # if self.x > self.screen.get_width - 25:
-        #     self.x = self.screen.get_width - 25
-        # if self.x < 25:
-        #     self.x = 25
-        # if self.y > self.screen.get_height - 25:
-        #     self.y = self.screen.get_height - 25
-        # if self.y < 25:
-        #     self.y = 25
+        if self.x > self.screen.get_width() - self.size:
+            self.x = self.screen.get_width() - self.size
+        if self.x < 0:
+            self.x = 0
+        if self.y > self.screen.get_height() - self.size:
+            self.y = self.screen.get_height() - self.size
+        if self.y < 0:
+            self.y = 0
 
-        pass
+
     def draw(self):
-        pygame.draw.rect(self.screen, (0, 0, 0), (self.x, self.y, 25, 25))
-        pass
+        pygame.draw.rect(self.screen, (255, 255, 0), (self.x, self.y, self.size, self.size))
+        pygame.draw.rect(self.screen, (152, 144, 2), (self.x, self.y, self.size, self.size), 4)
+
 
 def main():
     # turn on pygame
@@ -33,10 +35,10 @@ def main():
 
     # create a screen
     pygame.display.set_caption("No PDA!")
-    # TODO: Change the size of the screen as you see fit!
+    # Done: Change the size of the screen as you see fit!
     screen = pygame.display.set_mode((1000, 700))
 
-    p1 = Player(screen, 50, 50)
+    p1 = Player(screen, 55, 55)
 
     # let's set the framerate
     clock = pygame.time.Clock()
@@ -60,6 +62,23 @@ def main():
 
         # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, (98, 226, 108), (0, 0, 150, 150))
+        pygame.draw.rect(screen, (98, 226, 108), (screen.get_width()-150, screen.get_height()-150, 150, 150))
+        pygame.draw.rect(screen, (0, 0, 0), (150,0, 50, 550))
+        pygame.draw.rect(screen, (0, 0, 0), (screen.get_width()-200, screen.get_height()-550, 50, 550))
+        pygame.draw.circle(screen, (0, 0, 255), (275, 16), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (375, 16), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (475, 16), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (575, 16), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (675, 16), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (775, 16), 15)
+
+        pygame.draw.circle(screen, (0, 0, 255), (225, screen.get_height() - 15), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (325, screen.get_height() - 15), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (425, screen.get_height() - 15), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (525, screen.get_height() - 15), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (625, screen.get_height() - 15), 15)
+        pygame.draw.circle(screen, (0, 0, 255), (725, screen.get_height() - 15), 15)
 
         p1.draw()
 
@@ -69,3 +88,4 @@ def main():
         pygame.display.update()
 
 main()
+
